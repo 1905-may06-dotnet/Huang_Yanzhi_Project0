5c51b799d6643f60d6f6f10352f89ecbd2df93c2
evanh2015 <evanhuang10@yahoo.com>
2019-05-07 13:36:21 -0500

    2nd change

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿//Namespace - logical collection of libraries
+//1. Predefined name space
+using System; 
 
-namespace BasicOfCSharp
+//2. User Defined namespace
+namespace BasicsOfCSharp
 {
+    //Types : Class, Structs, Enums, interfaces, Delegates
     class Program
     {
+        //Type Members: Methods, Variables, Constants, Properties
+        static string name= "";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please Enter your name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "+name);
         }
     }
 }
